bffb58cd86119516dee07a16d8255067c37c643b
tobiafunes05 <tobiasfunes87@gmail.com>
2026-04-03 17:30:17 -0300

    Update Tp_Progamacion_Avanzada.java

--- Tp_Progamacion_Avanzada.java
+++ Tp_Progamacion_Avanzada.java
@@ -88,3 +88,55 @@ public class Tp_Progamacion_Avanzada {
         
     }
 }
+
+package com.mycompany.tp_progamacion_avanzada;
+
+/**
+ *
+ * @author nvara
+ */
+class Libro {
+   
+    
+    String titulo;
+    String autor;
+    boolean disponible;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = true;
+    }
+    
+    @Override
+    public String toString(){
+        return "Titulo: " + titulo + ", Autor: " + autor;
+    }
+    
+    
+    public String estado() {
+    if (disponible) {
+        return " Disponible";
+    } else {
+        return " No disponible";
+    }
+}
+    public void DevolverLibro(){
+        if (disponible == false){
+            disponible=true;
+            System.out.println("Se devolvio el libro correctamente ");
+            
+        }else{
+            System.out.println("El libro no fue devuelto");
+        }
+    }
+    
+    public void PedirLibro(){
+        if (disponible){
+        disponible=false;
+        System.out.println("Libro prestado correctamente");
+        } else{
+            System.out.println("El libro ya esta prestado ");        
+        }           
+    }
+}
