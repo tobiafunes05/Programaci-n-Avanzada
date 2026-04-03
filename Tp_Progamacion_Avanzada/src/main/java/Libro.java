e3b49db689c86bda702c661643bfe82a7735d46e
tobiafunes05 <tobiasfunes87@gmail.com>
2026-04-03 17:25:26 -0300

    Add files via upload

---
+++ Tp_Progamacion_Avanzada/src/main/java/Libro.java
@@ -0,0 +1,24 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nvara
+ */
+class Libro {
+  
+    
+      String titulo;
+    String autor;
+    boolean disponible;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = true;
+    }
+    
+
+}
